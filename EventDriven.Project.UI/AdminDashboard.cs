516682b47168f94c14711186c8bea2f66b0e4762
troydivinot-hash <troydivinot@gmail.com>
2025-09-09 14:53:12 +0800

    Dampor/Delos Reyes Created Dashboad

---
+++ EventDriven.Project.UI/AdminDashboard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventDriven.Project.UI
+{
+    public partial class AdminDashboard : Form
+    {
+        public AdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdminDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void adminstinfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
